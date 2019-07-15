56de3c8744ef8f067582f8ad19be00229aaf26dc
Nur Alfian Julianda <nur.julianda@DDC.LOCAL>
2019-07-15 17:13:44 +0700

    first commit

---
+++ app/src/main/java/com/nalfian/forecast/ui/setting/SettingFragment.kt
@@ -0,0 +1,8 @@
+package com.nalfian.forecast.ui.setting
+
+import androidx.fragment.app.Fragment
+
+class SettingFragment: Fragment() {
+
+
+}
---
+++ app/src/main/java/com/nalfian/forecast/ui/weather/current/CurrentWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.nalfian.forecast.ui.weather.current
+
+import androidx.lifecycle.ViewModel;
+
+class CurrentWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
